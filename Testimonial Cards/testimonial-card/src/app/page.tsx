1131d9390430ad2048d959dbcce34b3389a3d97d
evadantir <evadantir@gmail.com>
2024-12-25 15:18:53 +0700

    ♻️ refactor code

--- Testimonial Cards/testimonial-card/src/app/page.tsx
+++ Testimonial Cards/testimonial-card/src/app/page.tsx
@@ -1,5 +1,4 @@
-import Credits from "@/src/components/Credits";
-import Image from "next/image";
+import TestimonialCard from "../components/TestimonialCard";
 
 export default function Home() {
   const data = {
@@ -10,35 +9,11 @@ export default function Home() {
   };
 
   return (
-    <div className="w-full h-full flex flex-col items-center bg-gradient-to-b from-gray-50 to-[#d2d6db] py-[200px] ">
-      <main className="max-w-[340px] flex flex-col gap-4 bg-white p-6 rounded-lg shadow-md">
-        <div className="flex justify-center items-center gap-4 self-stretch">
-          {/* Avatar section */}
-          <figure className="w-12 h-12 md:w-16 md:h-16">
-            <Image
-              src={data?.avatar}
-              width={48}
-              height={48}
-              className="w-full h-full object-cover"
-              alt={`${data?.name}'s Avatar`}
-            />
-          </figure>
-          {/* Name & Username section */}
-          <section className="flex flex-col gap-px truncate text-pretty">
-            <span className="font-semibold text-lg text-justify text-neutral-900 ">
-              {data?.name}
-            </span>
-            <span className="font-normal text-sm text-neutral-600">
-              {data?.userName}
-            </span>
-          </section>
-        </div>
-        {/* Bio section */}
-        <p className="font-normal text-base text-neutral-600 \\">
-          {data?.bio}
-        </p>
-      </main>
-      <Credits />
-    </div>
+    <TestimonialCard 
+      name={data.name}
+      userName={data.userName}
+      bio={data.bio}
+      avatar={data.avatar}
+    />
   );
 }
