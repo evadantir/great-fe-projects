5c0faa0cc6ae224dda5888d315c7c0809585fcd2
evadantir <evadantir@gmail.com>
2024-12-25 15:21:40 +0700

    🎨 ✨ Add ESLint configuration and refactor TestimonialCard component
    - Introduced ESLint configuration for improved code quality and consistency.
    - Refactored TestimonialCard component for better readability and structure, including consistent use of TypeScript types and formatting.

--- Testimonial Cards/testimonial-card/src/components/TestimonialCard.tsx
+++ Testimonial Cards/testimonial-card/src/components/TestimonialCard.tsx
@@ -1,46 +1,47 @@
 import Image from "next/image";
-import Credits from "./Credits";
 
 type TestimonialProps = {
-    userName: string,
-    avatar: string,
-    bio: string,
-    name: string,
-}
+  userName: string;
+  avatar: string;
+  bio: string;
+  name: string;
+};
 
-const TestimonialCard: React.FC<TestimonialProps> = ({userName, avatar, bio, name}:TestimonialProps) => {
-    return(
-        <div className="w-full h-full flex flex-col items-center bg-gradient-to-b from-gray-50 to-[#d2d6db] py-[200px] ">
-        <main className="max-w-[340px] flex flex-col gap-4 bg-white p-6 rounded-lg shadow-md">
-          <div className="flex justify-center items-center gap-4 self-stretch">
-            {/* Avatar section */}
-            <figure className="w-12 h-12 md:w-16 md:h-16">
-              <Image
-                src={avatar}
-                width={48}
-                height={48}
-                className="w-full h-full object-cover"
-                alt={`${name}'s Avatar`}
-              />
-            </figure>
-            {/* Name & Username section */}
-            <section className="flex flex-col gap-px truncate text-pretty">
-              <span className="font-semibold text-lg text-justify text-neutral-900 ">
-                {name}
-              </span>
-              <span className="font-normal text-sm text-neutral-600">
-                {userName}
-              </span>
-            </section>
-          </div>
-          {/* Bio section */}
-          <p className="font-normal text-base text-neutral-600">
-            {bio}
-          </p>
-        </main>
-        <Credits />
-      </div>
-    )
-}
+const TestimonialCard: React.FC<TestimonialProps> = ({
+  userName,
+  avatar,
+  bio,
+  name,
+}: TestimonialProps) => {
+  return (
+    <div className="w-full h-full flex flex-col items-center bg-gradient-to-b from-gray-50 to-[#d2d6db] py-[200px] ">
+      <main className="max-w-[340px] flex flex-col gap-4 bg-white p-6 rounded-lg shadow-md">
+        <div className="flex justify-center items-center gap-4 self-stretch">
+          {/* Avatar section */}
+          <figure className="w-12 h-12 md:w-16 md:h-16">
+            <Image
+              src={avatar}
+              width={48}
+              height={48}
+              className="w-full h-full object-cover"
+              alt={`${name}'s Avatar`}
+            />
+          </figure>
+          {/* Name & Username section */}
+          <section className="flex flex-col gap-px truncate text-pretty">
+            <span className="font-semibold text-lg text-justify text-neutral-900 ">
+              {name}
+            </span>
+            <span className="font-normal text-sm text-neutral-600">
+              {userName}
+            </span>
+          </section>
+        </div>
+        {/* Bio section */}
+        <p className="font-normal text-base text-neutral-600">{bio}</p>
+      </main>
+    </div>
+  );
+};
 
 export default TestimonialCard;
