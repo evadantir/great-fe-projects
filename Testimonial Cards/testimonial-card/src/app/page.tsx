b04347321804f4dae8676ba914ab54aa61044cc6
evadantir <evadantir@gmail.com>
2024-12-25 15:54:43 +0700

    💄 ui: update ui

--- Testimonial Cards/testimonial-card/src/app/page.tsx
+++ Testimonial Cards/testimonial-card/src/app/page.tsx
@@ -2,7 +2,7 @@ import TestimonialCard from "../components/TestimonialCard";
 
 export default function Home() {
   const data = {
-    name: "Sarah Doleeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+    name: "Sarah Dole Valle Sandos Esmeralda Queen Santosa Romeo Julieette",
     userName: "@sarahdole",
     testimonial: "I've been searching for high-quality abstract images for my design projects, and I'm thrilled to have found this platform. The variety and depth of creativity are astounding!",
     avatar: "/img/profile-thumbnail.png",
--- Testimonial Cards/testimonial-card/src/components/TestimonialCard.tsx
+++ Testimonial Cards/testimonial-card/src/components/TestimonialCard.tsx
@@ -14,31 +14,33 @@ const TestimonialCard: React.FC<TestimonialProps> = ({
   name,
 }: TestimonialProps) => {
   return (
-    <section className="w-full h-full flex flex-col items-center bg-gradient-to-b from-gray-50 to-[#d2d6db] pt-[200px]">
+    <section className="w-full h-full flex flex-col items-center pt-[200px]">
       <div className="max-w-[340px] flex flex-col gap-4 bg-white p-6 rounded-lg shadow-md">
-        <section className="flex w-full justify-center items-center gap-4 ">
+        <section className="flex w-full gap-4 ">
           {/* Avatar section */}
           <figure className="w-12 h-12">
             <Image
               src={avatar}
               width={48}
               height={48}
-              className="w-full h-full object-cover"
+              objectFit="cover"
               alt={`${name}'s Avatar`}
             />
           </figure>
           {/* Name & Username section */}
-          <section className="flex flex-col gap-px">
-            <span className="font-semibold text-lg text-justify text-neutral-900 ">
+          <section className="flex flex-col gap-px text-pretty w-full">
+            <span className="font-semibold text-lg text-justify text-neutral-900 w-full">
               {name}
             </span>
-            <span className="font-normal text-sm text-neutral-600">
+            <span className="font-normal text-sm text-neutral-600 w-full">
               {userName}
             </span>
           </section>
         </section>
         {/* testimonial section */}
-        <p className="font-normal text-base text-neutral-600">{testimonial}</p>
+        <blockquote>
+            <p className="font-normal text-base text-neutral-600">{testimonial}</p>
+        </blockquote>
       </div>
     </section>
   );
