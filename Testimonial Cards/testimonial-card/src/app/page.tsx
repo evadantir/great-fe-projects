6bc40d4d00475ca9f8c8dfaf0023a57df74c2e61
evadantir <evadantir@gmail.com>
2024-12-25 15:10:41 +0700

    ✨ Add global styles, layout, and favicon for testimonial card project
    - Introduced global CSS styles using Tailwind CSS for consistent theming and layout.
    - Created a new layout component to structure the application with metadata.
    - Implemented a dynamic home page featuring user testimonials and a credits section.
    - Added a favicon for improved branding and user experience.

--- Testimonial Cards/testimonial-card/src/app/page.tsx
+++ Testimonial Cards/testimonial-card/src/app/page.tsx
@@ -1,9 +1,9 @@
-import Credits from "@/components/Credits";
+import Credits from "@/src/components/Credits";
 import Image from "next/image";
 
 export default function Home() {
   const data = {
-    name: "Sarah Dole",
+    name: "Sarah Doleeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
     userName: "@sarahdole",
     bio: "I've been searching for high-quality abstract images for my design projects, and I'm thrilled to have found this platform. The variety and depth of creativity are astounding!",
     avatar: "/img/profile-thumbnail.png",
@@ -11,8 +11,8 @@ export default function Home() {
 
   return (
     <div className="w-full h-full flex flex-col items-center bg-gradient-to-b from-gray-50 to-[#d2d6db] py-[200px] ">
-      <main className=" max-w-[340px] flex flex-col gap-4 bg-white p-6 rounded-lg">
-        <div className="flex items-center gap-4 self-stretch">
+      <main className="max-w-[340px] flex flex-col gap-4 bg-white p-6 rounded-lg shadow-md">
+        <div className="flex justify-center items-center gap-4 self-stretch">
           {/* Avatar section */}
           <figure className="w-12 h-12 md:w-16 md:h-16">
             <Image
@@ -24,8 +24,8 @@ export default function Home() {
             />
           </figure>
           {/* Name & Username section */}
-          <section className="flex flex-col gap-px grow">
-            <span className="font-semibold text-lg text-justify text-neutral-900">
+          <section className="flex flex-col gap-px truncate text-pretty">
+            <span className="font-semibold text-lg text-justify text-neutral-900 ">
               {data?.name}
             </span>
             <span className="font-normal text-sm text-neutral-600">
@@ -34,9 +34,9 @@ export default function Home() {
           </section>
         </div>
         {/* Bio section */}
-        <span className="font-normal text-base text-neutral-600">
+        <p className="font-normal text-base text-neutral-600 \\">
           {data?.bio}
-        </span>
+        </p>
       </main>
       <Credits />
     </div>
